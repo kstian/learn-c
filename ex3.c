f12a4867055d8b276d4e3d181c9e0fe365f1e43f
Ken Kustian <ken.kustian@gmail.com>
2015-11-09 08:31:20 +0000

    play arround with printf function

---
+++ ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int age = 10;
+	int height = 72;
+
+	printf("I am %+10.3d years old.\n", age);
+	printf("I am %+10.3d inches tall.\n", height);
+	
+	return 0;
+}
